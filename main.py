7a6f8c505aec47823a35dc54c42539ad37a87f59
maxime0010 <maxime0010@gmail.com>
2024-08-06 15:59:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,53 @@ token = os.getenv("BENZINGA_API_KEY")
 if not token:
     raise ValueError("No API key found in environment variables")
 
+mdp = os.getenv("MYSQL_MDP")
+if not mdp:
+    raise ValueError("No mdp  found in environment variables")
+
 from benzinga import financial_data
 
 bz = financial_data.Benzinga(token)
 price = bz.delayed_quote(company_tickers = "AAPL") 
 rating = bz.ratings(company_tickers = "AAPL")
 
+# Database connection
+db_config = {
+    'user': 'doadmin',
+    'password': mdp,
+    'host': 'db-mysql-nyc3-03005-do-user-4526552-0.h.db.ondigitalocean.com',
+    'database': 'defaultdb'
+}
+
+def insert_rating_data(rating_data):
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+        add_rating = ("INSERT INTO ratings "
+                      "(id, action_company, action_pt, adjusted_pt_current, adjusted_pt_prior, analyst, analyst_name, "
+                      "currency, date, exchange, importance, name, notes, pt_current, pt_prior, rating_current, "
+                      "rating_prior, ticker, time, updated, url, url_calendar, url_news) "
+                      "VALUES (%(id)s, %(action_company)s, %(action_pt)s, %(adjusted_pt_current)s, %(adjusted_pt_prior)s, "
+                      "%(analyst)s, %(analyst_name)s, %(currency)s, %(date)s, %(exchange)s, %(importance)s, %(name)s, "
+                      "%(notes)s, %(pt_current)s, %(pt_prior)s, %(rating_current)s, %(rating_prior)s, %(ticker)s, "
+                      "%(time)s, %(updated)s, %(url)s, %(url_calendar)s, %(url_news)s)")
+
+        for rating in rating_data["ratings"]:
+            cursor.execute(add_rating, rating)
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
 
 def main():
     try:
         print(bz.output(rating))
+        insert_rating_data(rating)
         exit_program()
                 
     except Exception as e:
