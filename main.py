8dca95ad2577810e9b456969a3c87e46ae8c71fe
maxime0010 <maxime0010@gmail.com>
2024-08-06 10:36:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ import os
 import requests
 import benzinga
 
-from benzinga import financial_data
-
 token = os.getenv("BENZINGA_API_KEY")
 if not token:
     raise ValueError("No API key found in environment variables")
 
+from benzinga import financial_data
+
 bz = financial_data.Benzinga(token)
 price = bz.delayed_quote(company_tickers = "AAPL") 
 print(bz.output(price))
+
+from benzinga import news_data
+
+bz_news = news_data.News(token)
+news = bz_news.news(company_tickers = "AAPL", 
+                    base_date = "2019-10-03")
+print(bz_news.output(news[-1])) #returns the most recent news item.
+
+exit()
