b2873d526757b44f0f6e9dd73e47c41e497a9fea
maxime0010 <maxime0010@gmail.com>
2024-08-06 10:26:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from benzinga import financial_data
 
 api_key = os.getenv("BENZINGA_API_KEY")
 if not api_key:
-    raise ValueError("No API key found in environment variables.")
+    raise ValueError("No API key found in environment variables")
 
 fin = financial_data.Benzinga(api_key)
 
