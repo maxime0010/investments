7616b0187fbf234126000bf3c10f7c4cfeb6b2ed
maxime0010 <maxime0010@gmail.com>
2024-08-18 14:20:33 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Number of days to consider an update as "recent"
+DAYS_RECENT = 21
+
+# Minimum success rate for high-performing analysts
+MIN_SUCCESS_RATE = 50
+
+# Minimum number of analysts covering a stock to include in the analysis
+MIN_ANALYSTS = 5
