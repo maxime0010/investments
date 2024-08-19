b1d6e4ce092dfdfd1d537efe15c7421703fc8ccd
maxime0010 <maxime0010@gmail.com>
2024-08-19 15:09:56 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 DAYS_RECENT = 21  # Number of days to define "recent"
-SUCCESS_RATE_THRESHOLD = 50  # Threshold for analysts' success rate
+SUCCESS_RATE_THRESHOLD = 0  # Threshold for analysts' success rate
 MIN_ANALYSTS = 3  # Minimum number of analysts covering a stock
