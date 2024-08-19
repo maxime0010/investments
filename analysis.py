4c77337ae0d46791e4315eed8734980313a1b1cf
maxime0010 <maxime0010@gmail.com>
2024-08-19 17:43:12 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -34,10 +34,13 @@ def calculate_price_target_statistics(cursor, closing_price_dict):
             AVG(CASE WHEN a.overall_success_rate > {SUCCESS_RATE_THRESHOLD} THEN r.adjusted_pt_current END) AS avg_high_success_analysts,
             COUNT(DISTINCT CASE WHEN r.date >= DATE_SUB(NOW(), INTERVAL {DAYS_RECENT} DAY) AND a.overall_success_rate > {SUCCESS_RATE_THRESHOLD} THEN r.analyst_name END) AS num_combined_criteria,
             STDDEV(CASE WHEN r.date >= DATE_SUB(NOW(), INTERVAL {DAYS_RECENT} DAY) AND a.overall_success_rate > {SUCCESS_RATE_THRESHOLD} THEN r.adjusted_pt_current END) AS stddev_combined_criteria,
-            AVG(CASE WHEN r.date >= DATE_SUB(NOW(), INTERVAL {DAYS_RECENT} DAY) AND a.overall_success_rate > {SUCCESS_RATE_THRESHOLD} 
-                     THEN (r.adjusted_pt_current - {closing_price_dict.get('ticker', 'NULL')}) / {closing_price_dict.get('ticker', 'NULL')} * 100 * a.overall_success_rate / 100 
+            SUM(CASE WHEN r.date >= DATE_SUB(NOW(), INTERVAL {DAYS_RECENT} DAY) AND a.overall_success_rate > {SUCCESS_RATE_THRESHOLD} 
+                     THEN (r.adjusted_pt_current - {closing_price_dict.get('ticker', 'NULL')}) / {closing_price_dict.get('ticker', 'NULL')} * 100 * a.overall_success_rate 
                      ELSE 0 
-                END) AS expected_return_combined_criteria
+                END) / SUM(CASE WHEN r.date >= DATE_SUB(NOW(), INTERVAL {DAYS_RECENT} DAY) AND a.overall_success_rate > {SUCCESS_RATE_THRESHOLD} 
+                                THEN a.overall_success_rate 
+                                ELSE 0 
+                          END) AS expected_return_combined_criteria
         FROM (
             SELECT 
                 ticker,
@@ -56,6 +59,7 @@ def calculate_price_target_statistics(cursor, closing_price_dict):
     """
     cursor.execute(query)
     return cursor.fetchall()
+
 
 def get_last_closing_price(cursor):
     query = """
