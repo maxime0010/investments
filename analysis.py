a72a26b62eab7ad39747cf94323877314ac490b2
maxime0010 <maxime0010@gmail.com>
2024-08-19 23:20:43 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -17,7 +17,7 @@ db_config = {
     'port': 25060
 }
 
-def calculate_price_target_statistics(cursor, closing_price_dict):
+def calculate_price_target_statistics(cursor):
     query = f"""
         SELECT 
             r.ticker,
@@ -34,7 +34,7 @@ def calculate_price_target_statistics(cursor, closing_price_dict):
             AVG(CASE WHEN a.overall_success_rate > {SUCCESS_RATE_THRESHOLD} THEN r.adjusted_pt_current END) AS avg_high_success_analysts,
             COUNT(DISTINCT CASE WHEN r.date >= DATE_SUB(NOW(), INTERVAL {DAYS_RECENT} DAY) AND a.overall_success_rate > {SUCCESS_RATE_THRESHOLD} THEN r.analyst_name END) AS num_combined_criteria,
             STDDEV(CASE WHEN r.date >= DATE_SUB(NOW(), INTERVAL {DAYS_RECENT} DAY) AND a.overall_success_rate > {SUCCESS_RATE_THRESHOLD} THEN r.adjusted_pt_current END) AS stddev_combined_criteria,
-            AVG(CASE WHEN r.date >= DATE_SUB(NOW(), INTERVAL {DAYS_RECENT} DAY) AND a.overall_success_rate > {SUCCESS_RATE_THRESHOLD} THEN (r.adjusted_pt_current - {closing_price_dict.get('ticker', 'NULL')}) / {closing_price_dict.get('ticker', 'NULL')} * 100 ELSE 0 END) AS expected_return_combined_criteria
+            AVG(CASE WHEN r.date >= DATE_SUB(NOW(), INTERVAL {DAYS_RECENT} DAY) AND a.overall_success_rate > {SUCCESS_RATE_THRESHOLD} THEN r.adjusted_pt_current END) AS avg_combined_criteria
         FROM (
             SELECT 
                 ticker,
