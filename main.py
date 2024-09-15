04e9905efb616c26ebd40d58f9f7e9681e10244b
maxime0010 <maxime0010@gmail.com>
2024-09-16 00:25:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ def run_script(script_name):
 
 
 # Execute the scripts in the specified order
-# run_script("price_target.py")
-# run_script("analysts.py")
-# run_script("stock_price.py")
+run_script("price_target.py")
+run_script("analysts.py")
+run_script("stock_price.py")
 # run_script("analysis.py")
 # run_script("portfolio.py")
 
