bac4ef8e8f412817ce243875585580fc88fffd46
maxime0010 <maxime0010@gmail.com>
2024-09-16 09:17:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,19 +11,19 @@ def run_script(script_name):
 
 # Execute the scripts in the specified order
 # run_script("price_target.py")
-# run_script("analysts.py")
-# run_script("stock_price.py")
 # run_script("analysis.py")
 # run_script("portfolio.py")
 
 
 
 # run_script("price_history.py")
-run_script("price_target_history.py")
 
 # run_script("portfolio_simulation.py")
 
 # Latest version
+# run_script("price_target_history.py")
+# run_script("analysts.py")
+# run_script("stock_price.py")
 # run_script("analysis_simulation.py")
 # run_script("portfolio.py")
 
